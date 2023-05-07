fcbe1f57211e54cd904468f429b455bbd4efa804
gokhanmeteerturk <me@e-mete.com>
2023-05-07 19:25:36 +0300

    update js

--- conf.js
+++ conf.js
@@ -1,4 +1,5 @@
 var site_conf = {
+    site_name:'Paslanmaz Tank',
     about_site:'Paslanmaz tanklar blogu, endüstriyel proseslerde kullanılan tank, mikseler, reaktör vb ekipmanlar' +
         'için ayrıntılı bilgiye ulaşmak isteyenler için oluşturuldu.<br>' +
         ' Bu blogda, paslanmaz tanklar başta olmak üzere,' +
