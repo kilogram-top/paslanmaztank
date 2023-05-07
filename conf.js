e8a8593b87aac1bbc0a56ad3629ebd7e67d6ad9c
gokhanmeteerturk <me@e-mete.com>
2023-05-07 18:54:03 +0300

    test

---
+++ conf.js
@@ -0,0 +1,7 @@
+var site_conf = {
+    about_site:'Paslanmaz tanklar blogu, endüstriyel proseslerde kullanılan tank, mikseler, reaktör vb ekipmanlar' +
+        'için ayrıntılı bilgiye ulaşmak isteyenler için oluşturuldu.<br>' +
+        ' Bu blogda, paslanmaz tanklar başta olmak üzere,' +
+        ' endüstriyel proses üniteleri ile ilgili çeşitli yazılar yayınlanmaktadır.' +
+        ' Reklam için aşağıdaki düğmeden iletişime geçebilirsiniz.'
+}
